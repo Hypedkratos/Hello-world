a3708aa33ac788cc069f3bd41f2f63a848a153a0
Hypedkratos <shashianand8651@gmail.com>
2024-11-05 20:39:38 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="main">
+      <span>Hello World</span>
+    </div>
+  );
+};
+
+export default App;
